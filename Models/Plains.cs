c7757b0bf289f838e7e4e99290cf69d7246c20b1
diana12wndb <dibragimova662@gmail.com>
2024-06-22 10:02:04 +0500

    Разработаны модели

---
+++ Models/Plains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EczamenV3.Models
+{
+    public class Plains
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Models/Reises.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EczamenV3.Models
+{
+    public class Reises
+    {
+        public int Id { get; set; }
+        public DateTime TimeVilet { get; set; }
+        public DateTime TimePrilet { get; set; }
+        public DateTime TimeOnFly { get; set; }
+        public DateTime DateVilet { get; set; }
+        public DateTime DatePrilet { get; set; }
+        public string PunktVilet { get; set; }
+        public string PunktPrilet { get; set; }
+        public int IdPlain { get; set; }
+    }
+}
